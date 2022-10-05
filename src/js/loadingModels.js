ed5a372b81e6d427c474c7d92be324cbc3993c02
Gyuri-v <fnffkdll@naver.com>
2022-10-06 00:41:09 +0900

    loadingmodels - clone 코딩 완료

--- src/js/loadingModels.js
+++ src/js/loadingModels.js
@@ -10,7 +10,7 @@ class BasicCharacterControls {
     _Init(params) {
         this._params = params;
         this._move = {
-            forwards: false,
+            forward: false,
             backward: false,
             left: false,
             right: false,
@@ -27,7 +27,7 @@ class BasicCharacterControls {
     _onKeyDown(event) {
         switch (event.keyCode) {
             case 87:  // w
-                this._move.forwards = true;
+                this._move.forward = true;
                 break;
             case 65: // a
                 this._move.left = true;
@@ -49,7 +49,7 @@ class BasicCharacterControls {
     _onKeyUp(event) {
         switch (event.keyCode) {
             case 87:  // w
-                this._move.forwards = false;
+                this._move.forward = false;
                 break;
             case 65: // a
                 this._move.left = false;
@@ -68,7 +68,8 @@ class BasicCharacterControls {
         }
     }
     
-    Update(timeInSeconds) {
+    Update(timeInSeconds) { 
+        // timeInSeconds = delta 값이라고 생각 / 약 0.01 정도
         const velocity = this._velocity;
         const frameDecceleration /* 프레임감속 */ = new THREE.Vector3(
             velocity.x * this._decceleration.x,
@@ -80,11 +81,54 @@ class BasicCharacterControls {
             Math.abs(frameDecceleration.z), Math.abs(velocity.z)
         )
         velocity.add(frameDecceleration);
+
+        const controlObject = this._params.target;
+        const _Q = new THREE.Quaternion();
+        const _A = new THREE.Vector3();
+        const _R = controlObject.quaternion.clone();
+
+        if ( this._move.forward ) {
+            velocity.z += this._acceleration.z * timeInSeconds;
+        }
+        if ( this._move.backward ) {
+            velocity.z -= this._acceleration.z * timeInSeconds;
+        }
+        if ( this._move.left ) {
+            _A.set(0, 1, 0);
+            _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * this._acceleration.y) // (어떤축을 기준으로 할건지, 각도) -- y 축을 기준으로, 뒤에 값만큼 회전
+            _R.multiply(_Q);
+        }
+        if ( this._move.right ) {
+            _A.set(0, 1, 0);
+            _Q.setFromAxisAngle(_A, -Math.PI * timeInSeconds * this._acceleration.y) // (어떤축을 기준으로 할건지, 각도) -- y 축을 기준으로, 뒤에 값만큼 회전
+            _R.multiply(_Q);
+        }
+
+        controlObject.quaternion.copy(_R);
+
+        const oldPosition = new THREE.Vector3();
+        oldPosition.copy(controlObject.position);
+
+        const forward = new THREE.Vector3(0, 0, 1);
+        forward.applyQuaternion(controlObject.quaternion);
+        forward.normalize();
+
+        const sideways = new THREE.Vector3(1, 0, 0);
+        sideways.applyQuaternion(controlObject.quaternion);
+        sideways.normalize();
+        
+        sideways.multiplyScalar(velocity.x * timeInSeconds);
+        forward.multiplyScalar(velocity.z * timeInSeconds);
+
+        controlObject.position.add(forward);
+        controlObject.position.add(sideways);
+
+        oldPosition.copy(controlObject.position);
     }
 }
 
 
-class LoadModel {
+class LoadModelDemo {
     constructor() {
         this._Init();
     }
@@ -155,6 +199,7 @@ class LoadModel {
 
         this._mixers = [];
         this._previousRAF = null;
+        this._clock = new THREE.Clock();
         
         this._LoadAnimationModel();
         this._RAF();
@@ -165,6 +210,97 @@ class LoadModel {
         loader.setPath('./resources/zombie/');
         loader.load('mremireh_o_desbiens.fbx', (fbx) => {
             fbx.scale.setScalar(0.1);
+            fbx.traverse(c => {
+                c.castShadow = true;
+            });
+        
+            const params = {
+                target: fbx,
+                camera: this._camera
+            }
+            this._controls = new BasicCharacterControls(params);
+            
+            const anim = new FBXLoader();
+            anim.setPath('./resources/zombie/');
+            anim.load('walk.fbx', (anim => {
+                const m = new THREE.AnimationMixer(fbx);
+                this._mixers.push(m);
+                const idle = m.clipAction(anim.animations[0]);
+                idle.play();
+            }));
+            this._scene.add(fbx);
+        });
+    }
+
+    _LoadAnimatedModleAndPlay(path, modelFile, animFile, offset) {
+        const loader = new FBXLoader();
+        loader.setPath(path);
+        loader.load(modelFile, (fbx) => {
+            fbx.scale.setScale(0.1);
+            fbx.traverse(c => {
+                c.castShadow = true;
+            });
+            fbx.position.copy(offset);
+
+            const anim = new FBXLoader();
+            anim.setPath(path);
+            anim.load(animFile, (anim) => {
+                const m = new THREE.AnimationMixer(fbx);
+                this._mixers.push(m);
+                const idle = m.clipAction(anim.animations[0]);
+                idle.play();
+            });
+            this._scene.add(fbx);
         })
     }
+
+    _LoadModel() {
+        const loader = new GLTFLoader();
+        loader.load('./resources/thing.glb', (gltf) => {
+            gltf.scene.traverse(c => {
+                c.castShadow = true;
+            });
+            this._scene.add(gltf.scene);
+        })
+    }
+
+    _OnWindowResize() {
+        this._camera.aspect = window.innerWidth / window.innerHeight;
+        this._camera.updateProjectionMatrix();
+        this._threejs.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    _RAF() {
+        
+        requestAnimationFrame((t) => {
+            
+            if ( this._previousRAF === null ) {
+                this._previousRAF = t;
+            }
+
+            this._RAF();
+
+            this._threejs.render(this._scene, this._camera);
+            this._Step(t - this._previousRAF);
+            this._previousRAF = t;
+        })
+    }
+
+    _Step(timeElapsed) {
+        const timeElapsedS = timeElapsed * 0.001;
+        // = clock.getDelta() 와 같이 raf 함수가 실행될때 마다의 시간 간격을 가져오는데, 콘솔에 찍어보니 더 안정적임
+        if (this._mixers) {
+            this._mixers.map(m => m.update(timeElapsedS));
+        };
+        
+        if (this._controls) {
+            this._controls.Update(timeElapsedS);
+        }
+    }
 }
+
+let _APP = null;
+
+window.addEventListener('DOMContentLoaded', () => {
+    _APP = new LoadModelDemo();
+})
