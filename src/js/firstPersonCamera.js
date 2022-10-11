fb3c954872b5f41142e4ba40809b187932e40c27
Gyuri-v <fnffkdll@naver.com>
2022-10-12 00:55:17 +0900

    first person camera - finish

--- src/js/firstPersonCamera.js
+++ src/js/firstPersonCamera.js
@@ -1,13 +1,17 @@
 import * as THREE from 'three';
 
-import { FirstPersonControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FirstPersonControls.js';
-import { ReinhardToneMapping } from 'three';
+import {
+  FirstPersonControls
+} from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FirstPersonControls.js';
+import {
+  ReinhardToneMapping
+} from 'three';
 
 const KEYS = {
-  a: 65,
-  s: 83,
-  w: 87,
-  d: 68,
+  'a': 65,	
+  's': 83,	
+  'w': 87,	
+  'd': 68,
 };
 
 function clamp(x, a, b) {
@@ -15,9 +19,208 @@ function clamp(x, a, b) {
   return Math.min(Math.max(x, a), b);
 }
 
+class InputController {
+  constructor(target) {
+    this.target_ = target || document;
+    this.initialize_();
+  }
+
+  initialize_() {
+    this.current_ = {
+      leftButton: false,
+      rightButton: false,
+      mouseXDelta: 0,
+      mouseYDelta: 0,
+      mouseX: 0,
+      mouseY: 0,
+    };
+    this.previous_ = null;
+    this.keys_ = {};
+    this.previousKeys_ = {};
+    this.target_.addEventListener('mousedown', (e) => this.onMouseDown_(e), false);
+    this.target_.addEventListener('mousemove', (e) => this.onMouseMove_(e), false);
+    this.target_.addEventListener('mouseup', (e) => this.onMouseUp_(e), false);
+    this.target_.addEventListener('keydown', (e) => this.onKeyDown_(e), false);
+    this.target_.addEventListener('keyup', (e) => this.onKeyUp_(e), false);
+  }
+
+  onMouseMove_(e) {
+    this.current_.mouseX = e.pageX - window.innerWidth / 2;
+    this.current_.mouseY = e.pageY - window.innerHeight / 2;
+
+    if (this.previous_ === null) {
+      this.previous_ = {
+        ...this.current_
+      }; // ???
+    }
+
+    this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+    this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+  }
+
+  onMouseDown_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = true;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = true;
+        break;
+      }
+    }
+  }
+
+  onMouseUp_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = false;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = false;
+        break;
+      }
+    }
+  }
+
+  onKeyDown_(e) {
+    this.keys_[e.keyCode] = true;
+  }
+
+  onKeyUp_(e) {
+    this.keys_[e.keyCode] = false;
+  }
+
+  key(keyCode) {
+    return !!this.keys_[keyCode];
+  }
+
+  isReady() {
+    return this.previous_ !== null;
+  }
+
+  update(_) {
+    if (this.previous_ !== null) {
+      this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+      this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+
+      this.previous_ = {
+        ...this.current_
+      }; // ???
+    }
+  }
+}
+
 class FirstPersonCamera {
-  constructor() {
-    // 
+  constructor(camera, objects) {
+    this.camera_ = camera;
+    this.input_ = new InputController();
+    this.rotation_ = new THREE.Quaternion();
+    this.translation_ = new THREE.Vector3(0, 2, 0);
+    this.phi_ = 0;
+    this.phiSpeed_ = 8;
+    this.theta_ = 0;
+    this.thetaSpeed_ = 5;
+    this.headBobActive_ = false;
+    this.headBobTimer_ = 0;
+    this.objects_ = objects;
+  }
+
+  update(timeElapsedS) {
+    this.updateRotation_(timeElapsedS);
+    this.updateCamera_(timeElapsedS);
+    this.updateTranslation_(timeElapsedS);
+    this.updateHeadBob_(timeElapsedS);
+    this.input_.update(timeElapsedS);
+  }
+
+  updateCamera_(_) {
+    // ??? 요기 전체적으로 다시 봐보기
+    // console.log(this.headBobTimer_);
+    this.camera_.quaternion.copy(this.rotation_);
+    this.camera_.position.copy(this.translation_);
+    this.camera_.position.y += Math.sin(this.headBobTimer_ * 10) * 1.5;
+
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(this.rotation_);
+
+    const dir = forward.clone();
+
+    forward.multiplyScalar(100);
+    forward.add(this.translation_);
+
+    let closest = forward;
+    const result = new THREE.Vector3();
+    const ray = new THREE.Ray(this.translation_, dir);
+    for (let i = 0; i < this.objects_.length; ++i) {
+      if (ray.intersectBox(this.objects_[i], result)) {
+        if (result.distanceTo(ray.origin) < closest.distanceTo(ray.origin)) {
+          closest = result.clone();
+        }
+      }
+    }
+
+    this.camera_.lookAt(closest);
+  }
+
+  updateHeadBob_(timeElapsedS) {
+    if (this.headBobActive_) {
+      const wavelength = Math.PI;
+      const nextStep = 1 + Math.floor(((this.headBobTimer_ + 0.000001) * 10) / wavelength); // ????
+      const nextStepTime = nextStep * wavelength / 10;
+      this.headBobTimer_ = Math.min(this.headBobTimer_ + timeElapsedS, nextStepTime);
+
+      if (this.headBobTimer_ == nextStepTime) {
+        this.headBobActive_ = false;
+      }
+    }
+  }
+
+  updateTranslation_(timeElapsedS) {
+    const forwardVelocity = (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0);
+    const strafeVelocity = (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0);
+
+    const qx = new THREE.Quaternion();
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(qx);
+    forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
+
+    const left = new THREE.Vector3(-1, 0, 0);
+    left.applyQuaternion(qx);
+    left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
+
+    this.translation_.add(forward);
+    this.translation_.add(left);
+
+    if (forwardVelocity != 0 || strafeVelocity != 0) {
+      this.headBobActive_ = true;
+    }
+  }
+
+  updateRotation_(timeElapsedS) {
+    const xh = this.input_.current_.mouseXDelta / window.innerWidth;
+    const yh = this.input_.current_.mouseYDelta / window.innerHeight;
+
+    this.phi_ += -xh * this.phiSpeed_;
+    this.theta_ = clamp(this.theta_ + -yh * this.thetaSpeed_, -Math.PI / 3, Math.PI / 3);
+
+    const qx = new THREE.Quaternion();
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+    const qz = new THREE.Quaternion();
+    qz.setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.theta_);
+
+    const q = new THREE.Quaternion();
+    q.multiply(qx);
+    q.multiply(qz);
+
+    this.rotation_.copy(q);
   }
 }
 
@@ -47,7 +250,7 @@ class FirstPersonCameraDemo {
   }
 
   initializeRenderer_() {
-    this.threejs_ = new THREE.WebGL1Renderer({
+    this.threejs_ = new THREE.WebGLRenderer({
       antialias: false,
     });
     this.threejs_.shadowMap.enabled = true;
@@ -59,13 +262,13 @@ class FirstPersonCameraDemo {
 
     document.body.appendChild(this.threejs_.domElement);
 
-    window.addEventListener('resize', () => { 
+    window.addEventListener('resize', () => {
       this.onWindowResize_();
     }, false);
 
-    const aspect = window.innerWidth/window.innerHeight;
+    const aspect = window.innerWidth / window.innerHeight;
     this.camera_ = new THREE.PerspectiveCamera(60, aspect, 1, 1000);
-    this.camera_.position.set(0, 0, 0);
+    this.camera_.position.set(0, 2, 0);
 
     this.scene_ = new THREE.Scene();
 
@@ -91,16 +294,18 @@ class FirstPersonCameraDemo {
     const mapLoader = new THREE.TextureLoader();
     const maxAnisotropy = this.threejs_.capabilities.getMaxAnisotropy();
     // maxAnisotropy : max 비등방성 | .getMaxAnisotropy() 사용 가능한 최대 이방성을 반환
-    const checkerboader = mapLoader.load('resources/checkerboard.png');
-    checkerboader.anisotropy = maxAnisotropy;
-    checkerboader.wrapS = THREE.RepeatWrapping;
-    checkerboader.wrapT = THREE.RepeatWrapping;
-    checkerboader.repeat.set(32, 32);
-    checkerboader.encoding = THREE.sRGBEncoding;
+    const checkerboard = mapLoader.load('resources/checkerboard.png');
+    checkerboard.anisotropy = maxAnisotropy;
+    checkerboard.wrapS = THREE.RepeatWrapping;
+    checkerboard.wrapT = THREE.RepeatWrapping;
+    checkerboard.repeat.set(32, 32);
+    checkerboard.encoding = THREE.sRGBEncoding;
 
     const plane = new THREE.Mesh(
       new THREE.PlaneGeometry(100, 100, 10, 10),
-      new THREE.MeshStandardMaterial({ map: checkerboader })
+      new THREE.MeshStandardMaterial({
+        map: checkerboard
+      })
     )
     plane.castShadow = false;
     plane.receiveShadow = true;
@@ -142,13 +347,13 @@ class FirstPersonCameraDemo {
     const wall4 = wall3.clone();
     wall4.position.set(-50, -40, 0);
     this.scene_.add(wall4);
-    
+
     const meshes = [plane, box, wall1, wall2, wall3, wall4]
-    
+
     this.objects_ = [];
 
-    for (let i = 0; i < meshes.length; i++) {
-      const b = new THREE.Box2();
+    for (let i = 0; i < meshes.length; ++i) {
+      const b = new THREE.Box3();
       b.setFromObject(meshes[i]); // ????
       this.objects_.push(b);
     }
@@ -156,12 +361,12 @@ class FirstPersonCameraDemo {
     // crosshair -- 표적!! 
     const crosshair = mapLoader.load('resources/crosshair.png');
     crosshair.anisotropy = maxAnisotropy;
-    
+
     this.sprite_ = new THREE.Sprite(
       new THREE.SpriteMaterial({
         map: crosshair,
         color: 0xffffff,
-        fog: false, 
+        fog: false,
         depthTest: false,
         depthWrite: false
       })
@@ -178,7 +383,7 @@ class FirstPersonCameraDemo {
     const penumbra = 0.5;
     const decay = 1;
 
-    let light = new THREE.SpotLight('#fff', distance, angle, penumbra, decay);f
+    let light = new THREE.SpotLight('#fff', distance, angle, penumbra, decay);
     light.castShadow = true;
     // ????
     light.shadow.bias = -0.00001;
@@ -248,7 +453,7 @@ class FirstPersonCameraDemo {
     this.uiCamera_.left = -this.camera_.aspect;
     this.uiCamera_.right = this.camera_.aspect;
     this.uiCamera_.updateProjectionMatrix();
-    
+
     this.threejs_.setSize(window.innerWidth, window.innerHeight);
   }
 
@@ -257,21 +462,22 @@ class FirstPersonCameraDemo {
       if (this.previousRAF_ === null) {
         this.previousRAF_ = t;
       }
-      
+
       this.step_(t - this.previousRAF_);
       this.threejs_.autoClear = true;
       this.threejs_.render(this.scene_, this.camera_);
       this.threejs_.autoClear = false; // ???? 왜 또하는가?
-      this.threejs_.render(this.uiCamera_, this.uiCamera_);
+      this.threejs_.render(this.uiScene_, this.uiCamera_);
       this.previousRAF_ = t;
       this.raf_();
-    }) 
+    })
   }
 
   step_(timeElapsed) {
     const timeElapsedS = timeElapsed * 0.001;
     
-    this.fpsCamera_.update(timeElapsed);
+    // this.controls_.update(timeElapsedS);
+    this.fpsCamera_.update(timeElapsedS);
   }
 }
 
